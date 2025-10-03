4fee1c332356ca9bfe628e9e4f0bfc7e37ca4319
Anthony Tasayco <anthonyjts25@gmail.com>
2025-10-03 13:51:07 -0500

    Fix- add user admin

---
+++ src/apps/users/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as UA
+
+from src.apps.customers.forms import CustomerChangeForm, CustomerCreationForm
+from .models import (
+    User, )
+
+
+@admin.register(User)
+class CustomerAdmin(UA):
+
+    add_form = CustomerCreationForm
+    form = CustomerChangeForm
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password', 'uuid_hash')
+        }),
+        (_('Personal info'), {
+            'fields': ('generated_credential', 'names', 'last_name',
+                       'full_name', 'occupation', 'job_company',
+                       'company_position',
+                       'age_range', 'country', 'speciality',
+                       'allow_networking',
+                       'virtual', 'in_person')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser',
+                       'groups',
+                       'user_permissions')
+        }),
+        (_('Creacion'), {
+            'fields': ('last_login', 'date_joined')
+        })
+    )
+    add_fieldsets = ((None, {
+        'classes': ('wide', ),
+        'fields': ('email', 'password1', 'password2'),
+    }), )
+    list_display = ('email', 'names', 'document', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'names')
+    ordering = ('-modified', )
+    # actions = [export_as_excel_action(), ]
